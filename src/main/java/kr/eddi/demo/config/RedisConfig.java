a12500beba3bdc7201b735652e8b8c8c9c96a007
Jaekyoung Kim <worud7528@gmail.com>
2023-06-07 15:24:05 +0900

    Update RedisConfig.java

--- src/main/java/kr/eddi/demo/config/RedisConfig.java
+++ src/main/java/kr/eddi/demo/config/RedisConfig.java
@@ -11,34 +11,41 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.io.IOException;
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 
 @Configuration
 public class RedisConfig {
 
-    @Value("classpath:/redis.properties")
-    private Resource redisProperties;
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private String redisPort;
+    @Value("${spring.redis.password}")
+    private String redisPassword;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactory () {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        Properties props = new Properties();
-        try {
-            props.load(redisProperties.getInputStream());
-            configuration.setHostName(props.getProperty("spring.redis.host"));
-            configuration.setPort(Integer.parseInt(props.getProperty("spring.redis.port")));
-            configuration.setPassword(props.getProperty("spring.redis.password"));
-        } catch (IOException e) {
-            // 처리 코드 작성
-        }
+        configuration.setHostName(redisHost);
+        configuration.setPort(Integer.parseInt(redisPort));
+        configuration.setPassword(redisPassword);
 
         LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(configuration);
         return connectionFactory;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate () {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
